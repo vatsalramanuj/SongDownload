b9c503687c1f9171295512635e499be218e4293c
vatsalramanuj <128302464+vatsalramanuj@users.noreply.github.com>
2026-02-26 23:02:00 +0530

    Change default port from 7860 to 8080

--- app.py
+++ app.py
@@ -115,5 +115,5 @@ def serve_file(video_id):
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 7860))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
